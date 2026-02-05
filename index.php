a9ddbd96816ca197d7a5e47b6baf701308d187e7
MeJlukc <solomatin.yarik@mail.ru>
2026-02-05 18:34:37 +0300

    Layout (w/o MVC, PHP and DataBase)

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <img src="./images/logo.svg" alt="Logo" class="header-logo">
+            <p class="header-text">Галактический<br>
+                вестник
+            </p>
+        </div>
+    </header>
+    <main>
+        <section class="last-news">
+            <h1 class="last-news-title">Возвращение этнографа</h1>
+            <p class="last-news-text">Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</p>
+        </section>
+        <section class="news">
+            <h1 class="news-title">Новости</h1>
+            <div class="news-container">
+                <div class="news-item">
+                    <p class="news-item-date">11.06.2412</p>
+                    <h2 class="news-item-title">Возвращение этнографа</h2>
+                    <p class="news-item-text">Cегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</p>
+                    <a href="/" class="news-item-button-link">Подробнее</a>
+                </div>
+                <div class="news-item">
+                    <p class="news-item-date">11.06.2412</p>
+                    <h2 class="news-item-title">Несчастный случай с известной светской дивой</h2>
+                    <p class="news-item-text">Светская дива Алиса Уткина попала под троллейбус, пытаясь уйти от преследования.</p>
+                    <a href="/" class="news-item-button-link">Подробнее</a>
+                </div>
+                <div class="news-item">
+                    <p class="news-item-date">11.06.2412</p>
+                    <h2 class="news-item-title">Папа Римский совершил визит в систему Альфы Центавра</h2>
+                    <p class="news-item-text">Папа Римский Жан-Клод XIV посетил с апостольским визитом систему Альфы Центавра. Он принял участие в ряде благотворительных мероприятий и совершил богослужение в соборе Ван-Дамм-де-Ури.</p>
+                    <a href="/" class="news-item-button-link">Подробнее</a>
+                </div>
+                <div class="news-item">
+                    <p class="news-item-date">11.06.2412</p>
+                    <h2 class="news-item-title">На чемпионате по пустотной гребле победила команда с Фобоса</h2>
+                    <p class="news-item-text">В поясе астероидов на проходившем в последние выходные этапе системного чемпионата по пустотной гребле команда с Фобоса одержала убедительную победу.</p>
+                    <a href="/" class="news-item-button-link">Подробнее</a>
+                </div>
+            </div>
+            <ul class="pagination-group-list">
+                <li class="paginatin-item pagination-item-before">
+                    <a href="" class="pagination-button-link-before">
+                        <span class="pagination-before-arrow"></span>
+                    </a>
+                </li>
+                <li class="pagination-item">
+                    <a href="/" class="pagination-button-link">1</a>
+                </li>
+                <li class="pagination-item">
+                    <a href="/" class="pagination-button-link">2</a>
+                </li>
+                <li class="pagination-item">
+                    <a href="/" class="pagination-button-link">3</a>
+                </li>
+                <li class="paginatin-item pagination-item-next">
+                    <a href="" class="pagination-button-link-next">
+                        <span class="pagination-next-arrow"></span>
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p class="footer-text">© 2023 — 2412 «Галактический вестник»</p>
+    </footer>
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <img src="./images/logo.svg" alt="Logo" class="header-logo">
+            <p class="header-text">Галактический<br>
+                вестник
+            </p>
+        </div>
+    </header>
+    <main class="work-news-main">
+        <p class="work-news-path">Главная / <span class="grey-text-color">Возвращение этнографа</span></p>
+        <h1 class="work-news-title">Возвращение этнографа</h1>
+        <div class="work-news-container">
+            <div class="work-news-info-container">
+                <p class="work-news-date">11.06.2412</p>
+                <h2 class="work-news-announce">Cегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</h2>
+                <p class="work-news-content">Сегодня с Проксимы вернулась этнографическая экспедиция под управлением профессора Джона Голдрама из Майхартского университета. Экспедиция работала в джунглях Туманного континента и занималась изучением неконтактного племени двухвостых зюзюмов.
+                <br>
+                <br>
+                "Это племя называет себя Ракатука-Тум-Тум и крайне неохотно контактирует с пришельцами", - сказал Джон Голдрам. На вопрос о том, каковы они - неконтактные зюзюмы, профессор заявил, что неконтактные зюзюмы такие же двухвостые и скандальные как и контактные, только неконтактные.
+                <br>
+                <br>
+                Ждем от профессора Голдрама новых интересных подробностей!
+                </p>
+                <!-- <a href="/" class="work-news-back-button">Назад к новостям</a> -->
+                <a class="work-news-back-button" href="/index.php">
+                    <span class="arrow-left"></span>Назад к новостям
+                </a>
+            </div>
+            <div class="work-news-image-container">
+                <img src="./images/background.jpg" alt="Selected news image" class="work-news-image">
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p class="footer-text">© 2023 — 2412 «Галактический вестник»</p>
+    </footer>
+</body>
+</html>
