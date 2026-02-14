1940dc621f79248e24d4064ef75b6cf118b47d9f
MeJlukc <solomatin.yarik@mail.ru>
2026-02-14 23:08:03 +0300

    updated file paths

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,6 +1,6 @@
 <?php
-require 'models/News.php';
-require 'Pagination.php';
+require ROOT . '/models/News.php';
+require ROOT . '/controllers/Pagination.php';
 
 class NewsController
 {
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require 'config/db.php';
-require 'controllers/NewsController.php';
+define('ROOT', __DIR__);
+
+require ROOT . '/config/db.php';
+require ROOT . '/controllers/NewsController.php';
 
 $controller = new NewsController($db);
 
